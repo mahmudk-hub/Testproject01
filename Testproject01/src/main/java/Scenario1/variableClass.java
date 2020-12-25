66467a22bd1e0184f6321e91db5678a40da8120a
kmahmud <kmahmud@mahmudhp>
2020-12-25 01:04:51 -0500

    package scenario1 and class variable created

---
+++ Testproject01/src/main/java/Scenario1/variableClass.java
@@ -0,0 +1,14 @@
+package Scenario1;
+
+public class variableClass {
+
+	public static void main(String[] args) {
+		int i = 5;
+		float f = 101.00f;
+		Float F = new Float(i);
+		System.out.println(i+f);
+		
+
+	}
+
+}
